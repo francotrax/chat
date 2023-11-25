941e0eb41f443706439149eed94cf99b126e4119
Franjo <franjo95@gmail.com>
2023-11-25 09:49:28 +0100

    test

--- main.py
+++ main.py
@@ -1,42 +1,71 @@
-import streamlit as st
-import openai
+# import streamlit as st
+# import openai
+# import os
+
+# from dotenv import load_dotenv
+
+# openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# st.title("Yazo Chat")
+
+# if "messages" not in st.session_state:
+#     st.session_state.messages = []
+
+# for message in st.session_state["messages"]:
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
+
+# # Initialize model
+# if user_prompt := st.chat_input("Your prompt"):
+#     st.session_state.messages.append({"role": "user", "content": user_prompt})
+#     with st.chat_message("user"):
+#         st.markdown(user_prompt)
+
+#     # generate responses
+#     with st.chat_message("assistant"):
+#         message_placeholder = st.empty()
+#         full_response = ""
+
+#         for response in openai.ChatCompletion.create(
+#             model=st.session_state.model,
+#             messages=[
+#                 {"role": m["role"], "content": m["content"]}
+#                 for m in st.session_state.messages
+#             ],
+#             stream=True,
+#         ):
+#             full_response += response.choices[0].delta.get("content", "")
+#             message_placeholder.markdown(full_response + "▌")
+
+#         message_placeholder.markdown(full_response)
+
+#     st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+
 import os
-
 from dotenv import load_dotenv
+from langchain.llms import OpenAI
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.callbacks import StreamlitCallbackHandler
+import streamlit as st
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
+load_dotenv()
 
-st.title("Yazo Chat")
+llm = OpenAI(temperature=0, streaming=True, openai_api_key=os.getenv("OPENAI_API_KEY"))
+tools = load_tools(["ddg-search"])
+agent = initialize_agent(
+    tools,
+    llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    # verbose=True
+)
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state["messages"]:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-# Initialize model
-if user_prompt := st.chat_input("Your prompt"):
-    st.session_state.messages.append({"role": "user", "content": user_prompt})
-    with st.chat_message("user"):
-        st.markdown(user_prompt)
-
-    # generate responses
+# try: "what are the names of the kids of the 44th president of america"
+# try: "top 3 largest shareholders of nvidia"
+if prompt := st.chat_input():
+    st.chat_message("user").write(prompt)
     with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-
-        for response in openai.ChatCompletion.create(
-            model=st.session_state.model,
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-
-        message_placeholder.markdown(full_response)
-
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+        st.write("🧠 thinking...")
+        st_callback = StreamlitCallbackHandler(st.container())
+        response = agent.run(prompt, callbacks=[st_callback])
+        st.write(response)
